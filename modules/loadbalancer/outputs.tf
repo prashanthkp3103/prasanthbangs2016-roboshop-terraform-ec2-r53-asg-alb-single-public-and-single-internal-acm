a9f61646bc58de29d148fdf35772314d169a640f
prasanthbangs2016 <prasanthbangs@gmail.com>
2024-10-01 22:21:58 +0530

    updated

--- modules/loadbalancer/outputs.tf
+++ modules/loadbalancer/outputs.tf
@@ -2,6 +2,6 @@ output "dns_name" {
   value = aws_lb.lb.dns_name
 }
 
-output "listener_arn" {
-  value = aws_lb_listener.main.arn
-}
+# output "listener_arn" {
+#   value = aws_lb_listener.public-https.arn
+# }
